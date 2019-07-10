0c7ace26f6cc994d8261c46ef5190d82687a20e0
Alberto Garcia-Muñoz <alberto.garciamunoz@vodafone.com>
2019-07-10 13:10:23 +0200

    adding text field logic

--- HackatonCarsharing/Screens/CreateTrip/Presenter/CreateTicketPresenter.swift
+++ HackatonCarsharing/Screens/CreateTrip/Presenter/CreateTicketPresenter.swift
@@ -9,7 +9,8 @@
 import Foundation
 
 protocol CreateTicketPresenterProtocol {
-    
+    func inputValuesUpdated()
+    func viewIsReady()
 }
 
 class CreateTicketPresenter {
@@ -20,8 +21,43 @@ class CreateTicketPresenter {
         self.view = view
     }
     
+    private func fetchTripModel() -> Trip? {
+        let origin = view?.origin ?? ""
+        let contactName = view?.contactName ?? ""
+        let contactNumber = view?.contactNumber ?? ""
+        let email = view?.email ?? ""
+        let totalSeats = Int(view?.totalSeats ?? "")
+        let stops = view?.stops
+        
+        guard !origin.isEmpty,!contactName.isEmpty, !contactNumber.isEmpty, !email.isEmpty, let seats = totalSeats, let date = buildDate(from: view?.startTime, date: view?.startDate) else { return nil }
+        
+        let dateStr = DateFormatter.dateFormatter.string(from: date)
+        return Trip(origin: origin, contact: contactNumber,
+                    name: contactName, email: email,
+                    totalSeats: seats, availableSeats: seats,
+                    startTime: dateStr, stops: stops)
+    }
+    
+    private func buildDate(from hour: Date?, date: Date?) -> Date? {
+        guard let hour = hour, let date = date else { return nil }
+        let calendar = Calendar.init(identifier: .gregorian)
+        var dateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        var hourComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: hour)
+        // Change the time to 9:30:00 in your locale
+        dateComponents.hour = hourComponents.hour
+        dateComponents.minute = hourComponents.minute
+        dateComponents.second = hourComponents.second
+        
+        return calendar.date(from: dateComponents)
+    }
 }
 
 extension CreateTicketPresenter: CreateTicketPresenterProtocol {
+    func inputValuesUpdated() {
+        view?.isContinueEnabled = fetchTripModel() != nil
+    }
     
+    func viewIsReady() {
+        view?.isContinueEnabled = fetchTripModel() != nil
+    }
 }
--- HackatonCarsharing/Screens/CreateTrip/View/CreateTicketViewController.swift
+++ HackatonCarsharing/Screens/CreateTrip/View/CreateTicketViewController.swift
@@ -9,11 +9,20 @@
 import UIKit
 
 protocol CreateTicketViewControllerProtocol : AnyObject {
-    
+    var origin: String? { get }
+    var contactName: String? { get }
+    var contactNumber: String? { get }
+    var email: String? { get }
+    var totalSeats: String? { get }
+    var startTime: Date? { get }
+    var startDate: Date? { get }
+    var stops: String? { get }
+    var isContinueEnabled: Bool { get set }
 }
 
 class CreateTicketViewController: UIViewController {
-
+    static let datePickerHeight: CGFloat = 216.0
+    
     var presenter: CreateTicketPresenterProtocol!
     
     @IBOutlet weak var titleFirstSectionLabel: UILabel! {
@@ -30,55 +39,67 @@ class CreateTicketViewController: UIViewController {
         didSet {
             contactNameTextField.setPlaceholder(text: "Nombre y apellido")
             contactNameTextField.delegate = self
+            contactNameTextField.addTarget(self, action: #selector(textFieldDidChanged(_:)), for: .editingChanged)
         }
     }
     @IBOutlet weak var corporateEmailTextField: CustomTextField!{
         didSet {
             corporateEmailTextField.setPlaceholder(text: "Email corporativo")
             corporateEmailTextField.delegate = self
+            corporateEmailTextField.keyboardType = .emailAddress
+            corporateEmailTextField.addTarget(self, action: #selector(textFieldDidChanged(_:)), for: .editingChanged)
         }
     }
     @IBOutlet weak var contactNumberTextField: CustomTextField!{
         didSet {
             contactNumberTextField.setPlaceholder(text: "Teléfono")
             contactNumberTextField.delegate = self
+            contactNumberTextField.keyboardType = .phonePad
+            contactNumberTextField.addTarget(self, action: #selector(textFieldDidChanged(_:)), for: .editingChanged)
         }
     }
     @IBOutlet weak var startLocation: CustomTextField!{
         didSet {
             startLocation.setPlaceholder(text: "Punto de salida")
             startLocation.delegate = self
+            startLocation.addTarget(self, action: #selector(textFieldDidChanged(_:)), for: .editingChanged)
         }
     }
     @IBOutlet weak var stopsTextField: CustomTextField!{
         didSet {
             stopsTextField.setPlaceholder(text: "Paradas intermedias")
             stopsTextField.delegate = self
+            stopsTextField.addTarget(self, action: #selector(textFieldDidChanged(_:)), for: .editingChanged)
         }
     }
-    @IBOutlet weak var startDate: DropdownCustomTextField!{
+    @IBOutlet weak var startDateTextField: DropdownCustomTextField!{
         didSet {
-            startDate.setPlaceholder(text: "Fecha de salida")
-            startDate.delegate = self
-            startDate.dropShadow()
+            startDateTextField.setPlaceholder(text: "Fecha de salida")
+            startDateTextField.delegate = self
+            startDateTextField.addTarget(self, action: #selector(textFieldDidChanged(_:)), for: .editingChanged)
         }
     }
     @IBOutlet weak var startTimeTextField: DropdownCustomTextField!{
         didSet {
             startTimeTextField.setPlaceholder(text: "Hora de salida")
             startTimeTextField.delegate = self
-            startTimeTextField.dropShadow()
+            startTimeTextField.addTarget(self, action: #selector(textFieldDidChanged(_:)), for: .editingChanged)
         }
     }
-    @IBOutlet weak var seatsTextField: DropdownCustomTextField!{
+    @IBOutlet weak var seatsTextField: CustomTextField! {
         didSet {
             seatsTextField.setPlaceholder(text: "Nº de plazas")
+            seatsTextField.keyboardType = .numberPad
             seatsTextField.delegate = self
-            seatsTextField.dropShadow()
+            seatsTextField.addTarget(self, action: #selector(textFieldDidChanged(_:)), for: .editingChanged)
         }
     }
     
     @IBOutlet weak var continueButton: UIButton!
+    let picker = UIDatePicker()
+    
+    private var startDateValue: Date?
+    private var startHourValue: Date?
     
     init() {
         super.init(nibName: nil, bundle: Bundle.init(for: type(of: self)))
@@ -88,10 +109,86 @@ class CreateTicketViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter.viewIsReady()
+    }
+    
+    func showHourPicker() {
+        //Formate Date
+        picker.datePickerMode = .time
+        //ToolBar
+        let toolbar = UIToolbar();
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneHourPicker));
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelDatePicker));
+        toolbar.setItems([doneButton,spaceButton,cancelButton], animated: false)
+        startTimeTextField.inputAccessoryView = toolbar
+        startTimeTextField.inputView = picker
+    }
+    
+    func showDatePicker(){
+        //Formate Date
+        picker.datePickerMode = .date
+        //ToolBar
+        let toolbar = UIToolbar();
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneDatePicker));
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelDatePicker));
+        toolbar.setItems([doneButton,spaceButton,cancelButton], animated: false)
+        startDateTextField.inputAccessoryView = toolbar
+        startDateTextField.inputView = picker
+    }
+    
+    @objc func doneDatePicker(){
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yy"
+        startDateValue = picker.date
+        startDateTextField.text = formatter.string(from: picker.date)
+        self.view.endEditing(true)
+    }
+    
+    @objc func doneHourPicker(){
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        startHourValue = picker.date
+        startTimeTextField.text = formatter.string(from: picker.date)
+        self.view.endEditing(true)
+    }
+    
+    @objc func cancelDatePicker(){
+        self.view.endEditing(true)
+    }
+    
+    @objc private func textFieldDidChanged(_ sender: UITextField) {
+        presenter.inputValuesUpdated()
+    }
+    
 }
 
 extension CreateTicketViewController: CreateTicketViewControllerProtocol {
+    var contactName: String? { return contactNameTextField.text }
     
+    var contactNumber: String? { return contactNumberTextField.text }
+    
+    var email: String? { return corporateEmailTextField.text }
+    
+    var totalSeats: String? { return seatsTextField.text }
+    
+    var startTime: Date? { return startHourValue }
+    
+    var startDate: Date? { return startDateValue }
+    
+    var stops: String? { return stopsTextField.text}
+    
+    var origin: String? { return startLocation.text }
+    
+    var isContinueEnabled: Bool {
+        set { continueButton.isEnabled = newValue }
+        get { return continueButton.isEnabled }
+    }
 }
 
 extension CreateTicketViewController: UITextFieldDelegate {
@@ -106,8 +203,8 @@ extension CreateTicketViewController: UITextFieldDelegate {
         case startLocation:
             stopsTextField.becomeFirstResponder()
         case stopsTextField:
-            startDate.becomeFirstResponder()
-        case startDate:
+            startDateTextField.becomeFirstResponder()
+        case startDateTextField:
             startTimeTextField.becomeFirstResponder()
         case startTimeTextField:
             seatsTextField.becomeFirstResponder()
@@ -116,5 +213,16 @@ extension CreateTicketViewController: UITextFieldDelegate {
             continueButton.sendActions(for: .touchUpInside)
         }
         return false
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        switch textField {
+        case startDateTextField:
+            showDatePicker()
+        case startTimeTextField:
+            showHourPicker()
+        default:
+            break
+        }
     }
 }
